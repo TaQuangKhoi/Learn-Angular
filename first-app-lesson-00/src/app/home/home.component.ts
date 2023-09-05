9002e16d58e1150d9c792110e19302ee976b4a86
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-05 11:52:09 +0700

    Create Home Component and Search Input

---
+++ first-app-lesson-00/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+  `,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
