9167c83aa2bf7e32df511348ee239fcaf298185f
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-05 12:32:06 +0700

    Try use Form

--- first-app-lesson-00/src/app/details/details.component.ts
+++ first-app-lesson-00/src/app/details/details.component.ts
@@ -3,11 +3,15 @@ import {CommonModule} from '@angular/common';
 import {ActivatedRoute} from '@angular/router';
 import {HousingService} from '../housing.service';
 import {HousingLocation} from '../housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   template: `
       <article>
           <img class="listing-photo" [src]="housingLocation?.photo"
@@ -24,6 +28,20 @@ import {HousingLocation} from '../housinglocation';
                   <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
               </ul>
           </section>
+          <section class="listing-apply">
+              <h2 class="section-heading">Apply now to live here</h2>
+              <form [formGroup]="applyForm" (submit)="submitApplication()">
+                  <label for="first-name">First Name</label>
+                  <input id="first-name" type="text" formControlName="firstName">
+
+                  <label for="last-name">Last Name</label>
+                  <input id="last-name" type="text" formControlName="lastName">
+
+                  <label for="email">Email</label>
+                  <input id="email" type="email" formControlName="email">
+                  <button type="submit" class="primary">Apply now</button>
+              </form>
+          </section>
       </article>
   `,
   styleUrls: ['./details.component.css']
@@ -33,8 +51,22 @@ export class DetailsComponent {
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
