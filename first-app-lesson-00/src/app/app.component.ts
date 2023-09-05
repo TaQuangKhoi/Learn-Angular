6a1c0799181313f1aadb902ac433109dfe5f4a4b
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-05 12:27:37 +0700

    Done details

--- first-app-lesson-00/src/app/app.component.ts
+++ first-app-lesson-00/src/app/app.component.ts
@@ -11,11 +11,13 @@ import { RouterModule } from '@angular/router';
   ],
   template: `
       <main>
-          <header class="brand-name">
-              <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-          </header>
+          <a [routerLink]="['/']">
+              <header class="brand-name">
+                  <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+              </header>
+          </a>
           <section class="content">
-            <router-outlet></router-outlet>
+              <router-outlet></router-outlet>
           </section>
       </main>
   `,
