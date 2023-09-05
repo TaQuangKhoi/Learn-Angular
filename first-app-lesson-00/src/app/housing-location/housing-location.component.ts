ab25f55268a4a54cfe0d30e72a23ccf55cb6eb5d
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-05 12:14:23 +0700

    Use service

--- first-app-lesson-00/src/app/housing-location/housing-location.component.ts
+++ first-app-lesson-00/src/app/housing-location/housing-location.component.ts
@@ -10,7 +10,7 @@ import {HousingLocation} from '../housinglocation';
       <section class="listing">
           <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
           <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-          <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+          <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
       </section>
   `,
   styleUrls: ['./housing-location.component.css']
