58ee18ad0fc3cd3ea4beeb9e4a44c8a31ecd7e7b
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-05 11:59:01 +0700

    Add an object of interface

--- first-app-lesson-00/src/app/home/home.component.ts
+++ first-app-lesson-00/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -23,5 +24,16 @@ import {HousingLocationComponent} from '../housing-location/housing-location.com
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
