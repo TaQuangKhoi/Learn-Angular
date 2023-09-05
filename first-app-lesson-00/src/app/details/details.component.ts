10fdba1461a4dec8064838bdddbbb02106428110
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-05 12:24:57 +0700

    get id from url param

--- first-app-lesson-00/src/app/details/details.component.ts
+++ first-app-lesson-00/src/app/details/details.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+import {HousingService} from '../housing.service';
+import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-details',
@@ -7,11 +10,16 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <p>
-      details works!
+      details works! {{ housingLocationId }}
     </p>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
 
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
