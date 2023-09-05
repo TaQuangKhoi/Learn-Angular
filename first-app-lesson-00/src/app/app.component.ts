d2b0fc94cf8eeb5878437d904a6592ac3af4d7e2
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-05 12:22:21 +0700

    use routes

--- first-app-lesson-00/src/app/app.component.ts
+++ first-app-lesson-00/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HomeComponent,
+    RouterModule,
   ],
   template: `
       <main>
@@ -13,7 +15,7 @@ import { HomeComponent } from './home/home.component';
               <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
           </header>
           <section class="content">
-              <app-home></app-home>
+            <router-outlet></router-outlet>
           </section>
       </main>
   `,
